5d675aa3caedbd23d675d1d2f2d5ca6440c80ce4
Taison Shea <71412637+LyraxH@users.noreply.github.com>
2023-05-04 14:41:36 +1200

    Initial commit, basic code, nothing works

---
+++ Game.java
@@ -0,0 +1,18 @@
+
+/**
+ * Creates terrain and gets me credits. I guess.
+ *
+ * Taison Shea
+ * 04/05/23 -> 
+ */
+import java.util.Scanner;
+import java.util.*;
+import java.util.ArrayList;
+public class Game
+{
+    Scanner input = new Scanner(System.in);
+    String breaker = "↓--------------------↓";
+    public Game(){
+         System.out.println("How big do you want the playing space to be?");
+    }
+}
