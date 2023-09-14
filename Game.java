0b5ea7ae8597fbdaf780fb342d5f31c409817aca
Taison Shea <71412637+LyraxH@users.noreply.github.com>
2023-09-15 09:38:10 +1200

    Adding main menu, kinda neat.

--- Game.java
+++ Game.java
@@ -14,14 +14,14 @@ public class Game
     // world building variables;
     int buffs;// how many buffs on the map
     int treasure; // how many treasure items on the map
-    int gridSize; // how big the playable map is
+    int gridSize = 0; // how big the playable map is
     int enemies; // how many enemies should show up
     int border; // where the border stands (one larger than playable map)
     int paths; // how many paths are required to make every area accessable
     int replaced = -1;
     int obtainables;
-    int testing = 0;
-    int checkingSS;
+    int testing = 0; // testing variable
+    int checkingSS; // also testing variable
     // misc variables
     Scanner input = new Scanner(System.in); // variable to get keyboard inputs.
     String breaker = "↓--------------------↓"; // just to split up text to make it more readable
@@ -36,20 +36,63 @@ public class Game
         obtainLocationY.clear(); //clears all prior y locations stored
         obtainPathX.clear(); //clears all prior x locations stored
         obtainPathY.clear(); //clears all prior y locations stored
-        InitializeBuffs();
+        Introduction(0);
         testing++;
         //System.out.println(testing);
         return;
     }
+    public void Introduction(int returning){
+        int choice;
+        System.out.println("\f");
+        if (returning == 1){
+            System.out.println("Please set the map parameters through: (2) Change Parameters");
+        } else if (returning == 2){
+            System.out.println("Map parameters Saved");
+        } else if (returning == 5){
+            System.out.println("Please enter a number according to the list");
+        }
+        System.out.println("<======Map Builder======>");
+        System.out.println("Worse than the other guys, don't get your hopes up");
+        System.out.println("<===What do you want to do?===>");
+        System.out.println("(1) Create Map");
+        System.out.println("(2) Change Parameters");
+        System.out.println("(3) Load Map");
+        System.out.println("(4) Exit");
+        try {
+            choice = input.nextInt();
+            if (choice == 1){
+                if (gridSize == 0){
+                    Introduction(1);
+                } else {
+                    CreateMap();
+                }
+            } else if (choice == 2){
+                InitializeBuffs();
+            } else if (choice == 3){
+                //LoadMap();
+            } else if (choice == 4){
+                System.exit(0);
+            } else {
+                input.nextLine();
+                Introduction(5);
+            }
+        } catch (Exception e){
+            input.nextLine();
+            Introduction(5);
+        }
+    }
     public void InitializeBuffs(){
+        System.out.println("\f");
         testing++;
         //System.out.println(testing);
         // sets parameters for the map based on what the player wants.
+        /**
         if (checkingSS  == 69){
             System.out.println("Buffs coming from surrounding Check");
         } else {
             System.out.println("buff from somewhere else");
         }
+        **/
         System.out.println("How many buffs do you want on the map? Recommendation: 1-2 Max: 10");
         try {
             buffs = 0;
@@ -89,13 +132,13 @@ public class Game
         enemies = gridSize / 2;
         border = gridSize + 1;
         paths = 4;
-        CreateMap();
+        Introduction(2);
         return;
     }
     public void CreateMap(){
         testing++;
-        System.out.println(testing);
-        System.out.println("Create Grid + Add Buffs");
+        //System.out.println(testing);
+        //System.out.println("Create Grid + Add Buffs");
         obtainLocationX.clear();
         obtainLocationY.clear();
         for (int i = 0; i < gridSize; i++){ // changes whats being changed for the y
@@ -135,8 +178,8 @@ public class Game
     }
     public void CreateBorders(){
         testing++;
-        System.out.println(testing);
-        System.out.println("Create Borders");
+        //System.out.println(testing);
+        //System.out.println("Create Borders");
         for (int i = 0; i < border; i++){ // spawns border along the x axis
             grid[i][gridSize] = "BORDER";
             //System.out.println("Placing border at " + i + " : " + border);
@@ -149,8 +192,8 @@ public class Game
     }
     public void SpawnLand(){
         testing++;
-        System.out.println(testing);
-        System.out.println("Initial Set Of Paths");
+        //System.out.println(testing);
+        //System.out.println("Initial Set Of Paths");
         //this is the annoying part that creates paths that bridge off of the obtainables making them all acessable.
         // BIGGEST PROBLEM, MAKING PATH SYSTEM THAT WORKS. LOTS OF TRIAL AND ERROR INCOMING
         for (int i = 0; i < obtainables; i++){ // goes through the location of all obtainables
@@ -234,8 +277,8 @@ public class Game
     }
     public void FillHoles(){
         testing++;
-        System.out.println(testing);
-        System.out.println("Fill Holes");
+        //System.out.println(testing);
+        //System.out.println("Fill Holes");
         try {
             for (int i = 1; i < (border-1); i++){ // changes whats being printed for the y axis
                 for (int t = 1; t < (border-1); t++){ // changes whats being printed for the x axis
@@ -301,8 +344,8 @@ public class Game
     }
     public void ConstructPaths(int x, int y){// will place pathss everywhere according to the ammount of paths needed.
         testing++;
-        System.out.println(testing);
-        System.out.println("Construct Path");
+        //System.out.println(testing);
+        //System.out.println("Construct Path");
         boolean up; // true means you can move, false means you cant
         boolean down; // true means you can move, false means you cant
         boolean left; // true means you can move, false means you cant
@@ -335,8 +378,8 @@ public class Game
     }
     public void AssignPath(int x,int y){ // checks surroundings, and if branching out will hit any of them, and then tells the initial calculation to stay away from branching to that side
         testing++;
-        System.out.println(testing);
-        System.out.println("Assign Path");
+        //System.out.println(testing);
+        //System.out.println("Assign Path");
         boolean up; // true means you can move, false means you cant
         boolean down; // true means you can move, false means you cant
         boolean left; // true means you can move, false means you cant
@@ -377,8 +420,8 @@ public class Game
     }
     public void DirectionRandomizer(boolean up, boolean down, boolean left, boolean right, int x, int y){
         testing++;
-        System.out.println(testing);
-        System.out.println("DirectionRandomizer");
+        //System.out.println(testing);
+        //System.out.println("DirectionRandomizer");
         int direction = rng.nextInt(3);
         if (direction == 0){ // up
             //System.out.println("Going up");
@@ -420,15 +463,15 @@ public class Game
     }
     public void PlacePath(int x, int y){
         testing++;
-        System.out.println(testing);
-        System.out.println("placed wall X " + x + " Y: " + y); 
+        //System.out.println(testing);
+        //System.out.println("placed wall X " + x + " Y: " + y); 
         grid[x][y] = "path";
         obtainPathX.add(x);
         obtainPathY.add(y);
         return;
     }
     public void DrawGame(){
-        //System.out.println("\f");
+        System.out.println("\f");
         for (int i = 0; i < border; i++){ // changes whats being printed for the y axis
             for (int t = 0; t < border; t++){ // changes whats being printed for the x axis
                 if (grid[t][i] == "path"){
@@ -443,7 +486,13 @@ public class Game
             }
             System.out.println(""); // next line
         }
+        NextSettings();
         //System.out.println(obtainLocationX);
         //System.out.println(obtainLocationY);
+    }
+    public void NextSettings(){ //Make it so they user can save the map state, (create an algorithm for this that works you dipshit. you can do it)
+        System.out.println("What do you want to do now?");
+        System.out.println(" (1) Save Map");
+        System.out.println(" (2) Fuck Off");
     }
 }
